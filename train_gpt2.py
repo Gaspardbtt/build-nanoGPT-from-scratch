c47bd295937afe11b1ef5c3b3424101e4b97a64a
Gaspard Botté <gaspardbotte@Air-de-Gaspard.lan>
2025-03-28 17:01:59 +0100

    correction bug wte=lm_head cf ATTENTION IS ALL U NEED / initialisation des poids par loi normale

--- train_gpt2.py
+++ train_gpt2.py
@@ -66,6 +66,7 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate='tanh')
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
 
     def forward(self,x):
         x = self.c_fc(x)
@@ -143,6 +144,24 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embd),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+        # weight sharing scheme
+        self.transformer.wte.weight = self.lm_head.weight
+
+        # initialize params
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
 
 
     def forward(self, idx, targets=None):
@@ -269,6 +288,10 @@ if torch.cuda.is_available():
     device = "cuda"
 print(f"using device: {device}")
 
+
+torch.manual_seed(13337)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(13337)
 
 train_loader = DataLoaderLite(B=4, T=32)
 
