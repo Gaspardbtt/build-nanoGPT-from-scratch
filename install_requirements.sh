379d211fd9070089f57a63ae2dc0c01d9af7b304
Gaspard Botté <gaspardbotte@Air-de-Gaspard.lan>
2025-04-10 20:59:03 +0200

    .sh to install all dependencies on lambda AI instance

---
+++ install_requirements.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Mettre à jour pip
+echo "Mise à jour de pip..."
+pip install --upgrade pip
+
+# Installer les dépendances
+echo "Installation des bibliothèques nécessaires..."
+
+pip install torch torchvision torchaudio
+pip install hellaswag
+pip install tiktoken
+pip install numpy
+pip install torchdistributed
+
+# Vérifier si les bibliothèques sont installées correctement
+echo "Vérification de l'installation des bibliothèques..."
+
+python -c "import torch; import hellaswag; import tiktoken; import numpy; import torch.distributed as dist; print('Tous les packages sont installés correctement')"
+
+echo "Installation terminée."
+
+
+# chmod +x install_requirements.sh pour rendre executable 
+
+# ./install_requirements.sh pour executer le script
